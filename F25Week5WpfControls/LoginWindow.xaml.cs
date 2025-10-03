1f6198b433a2290f3aebcfad5e93f8b3d2ac68eb
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-10-03 11:16:03 -0400

    complete

---
+++ F25Week5WpfControls/LoginWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace F25Week5WpfControls
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        string username = "abc";
+        string password = "abc";
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            txtUsername.Focus();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text.Trim() == username && pswPassword.Password.Trim() == password)
+            {
+                MainWindow win = new MainWindow();
+                win.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                txtUsername.Focus();
+            }
+        }
+    }
+}
